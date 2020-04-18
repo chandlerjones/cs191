8f1dde33fe78970a3c1f46482c2692759cf543e0
bradleyq <qu.bradley@gmail.com>
2020-04-18 12:25:53 -0700

    some logic errors

--- Gate.py
+++ Gate.py
@@ -4,9 +4,6 @@
 from Qubit import *
 import numpy as np
 
-# default noise model TBD
-default_noise = None
-
 # countermeasure for forseeable floating point issues when doing tensor products.
 def append_last_p(p):
     return np.append(p, [1.0 - sum(p)])
@@ -23,7 +20,7 @@ class Noise:
 
         No verifications / validations done atm
         """
-        self.t = np.append(transforms, [np.eye(n**2)])
+        self.t = np.concatenate([transforms, [np.eye(2**n)]])
         self.p = probabilities # not adding probability of I due to potential floating point issues
         self.k = len(self.p)
         self.n = n
@@ -35,10 +32,11 @@ class Noise:
         if not isinstance(other, Noise):
             raise TypeError('Cannot multiply type Gate with ' + type(other).__name__)
 
-        self.t = np.kron(self.t, other.t)
-        self.p = np.kron(append_last_p(self.p), append_last_p(other.p))[:-1] # remove I probability because...
-        self.k = len(self.p)
-        self.n += other.n
+        t = np.kron(self.t, other.t)
+        p = np.kron(append_last_p(self.p), append_last_p(other.p))[:-1] # remove I probability because...
+        n = self.n + other.n
+        return Noise(n, p, t)
+
 
     # Randomly chooses a noise transformation to be applied. Watch out for those nasty floating point errors!
     def eval(self):
@@ -48,10 +46,14 @@ class Noise:
         row_i = np.random.choice(len(self.t), p=append_last_p(self.p))
         return self.t[row_i]
 
+# No Noise class. Essentially an I matrix with probability 1
+class NoNoise(Noise):
+    def __init__(self, n):
+        super().__init__(n, np.empty(0), np.empty((0, 2**n, 2**n)))
 
 # Generic Gate class. by default it is Identity with default noise model
 class Gate:
-    def __init__(self, n, transform = np.eye(2), noise = None):
+    def __init__(self, n, transform = None, noise = None):
         """
         :n:          gate dimension
         :transform:  2^n x 2^n unitary matrix
@@ -59,6 +61,10 @@ class Gate:
 
         No verifications / validations done on transform(unitary) matrix atm
         """
+        if not noise:
+            noise = NoNoise(n)
+        if not transform:
+            transform = np.eye(n)
         self.u = transform
         self.noise = noise
         self.n = n
@@ -71,15 +77,10 @@ class Gate:
         if not isinstance(other, Gate):
             raise TypeError('Cannot multiply type Gate with ' + type(other).__name__)
 
-        self.u = np.kron(self.u, other.u)
-        if self.noise and other.noise:
-            self.noise *= other.noise
-        elif self.noise and other.noise:
-            pass
-        else:
-            self.noise = self.noise if self.noise else other.noise
-
-        self.n += other.n
+        u = np.kron(self.u, other.u)
+        t = self.noise * other.noise 
+        n = self.n + other.n
+        return Gate(n, u, t) # had to do it
 
     # Apply the gate to a given register.
     def apply(self, register):
