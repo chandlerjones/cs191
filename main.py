6f9453effc95f86a5647101a9156350a53f21b89
Chandler Jones <acjones98@berkeley.edu>
2020-04-19 14:56:15 -0700

    turns out I'm a dipshit sometimes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy
-import shors
+# import shors
 from qiskit import QuantumCircuit, QuantumRegister
 from Qubit import *
 
 
 def main():
+    print('################################################################')
     print('############ Noiseless Implementation Demonstration ############')
+    print('################################################################', '\n')
     print('We begin with single Qubit objects:')
     phi = Qubit()
     print('phi =', phi.__repr__(), '\n')
@@ -24,7 +26,7 @@ def main():
     y = Register(3)
     print('x =', x.__repr__())
     print('y =', y.__repr__(), '\n')
-    print('Create an equal superposition over all of the basis states with register x'
+    print('Create an equal superposition over all of the basis states with register x', '\n',
           ' and take the conjugate transpose of register y to make it a bra:')
     x = x.walsh()
     print('x =', x.__repr__())
@@ -33,14 +35,26 @@ def main():
     print('We expect the purity of x and y to be 1/8 (1/2^n, where n=3) and 1, respectively...')
     print('purity of x: ', x.purity)
     print('purity of y:', y.purity, '\n')
-    print('Taking their inner product (bra/ket) should result in the scalar 0, as these states are orthogonal. '
-          'Taking their outer product (ket/bra) should result in an 8x8 matrix operator...')
+    print('Taking their inner product (bra/ket) should result in a scalar value.', '\n',
+          'Taking their outer product (ket/bra) should result in an 8x8 matrix operator, which we omit for brevity',
+          '\n')
     p1 = y * x
     p2 = x * y
-    print('<y|x> =', p1)
-    print('|x><y| =', p2)
+    print('<y|x> =', p1, '\n')
+    # print('|x><y| =', p2, '\n', '\n')
 
+    print('Measuring our register will return the index of the state measured.', '\n'
+          'e.g.) if x.measure() returns 4, we have effectively measured the state |100>')
+    m = x.measure()
+    my = y.measure()
+    print('x after measurement: ', m,
+          '--> State measured: ', str(dec_to_bin(m)), '\n')
+    print('We should observe the zero state with certainty when measuring y:')
+    print('y after measurement: ', my, '\n', '\n')
+
+    print('############################################################################')
     print('############ Basic Functionality Tests Against Control (Qiskit) ############')
+    print("############################################################################")
 
     qis_x = QuantumCircuit(2, 2)
     qis_x.h(0)
@@ -50,8 +64,12 @@ def main():
     print(qis_x)
 
     x = Register(name='phi_plus')
-    print("Our bell state construction")
-    print(x.name, ': ', x.__repr__())
+    print("Our bell state construction:")
+    print(x.name, ': ', x.__repr__(), '\n', '\n')
+
+    print('###############################################################################################')
+    print('############ Test of Noiseless Implementation of Quantum Factoring Against Control ############')
+    print('###############################################################################################')
 
 
 if __name__ == "__main__":
